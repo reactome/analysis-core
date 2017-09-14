f37e4eef286981e83296da300b14bf1642bde2dc
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-09-14 14:08:18 +0100

    Interactors are treated as first class citizens

--- src/main/java/org/reactome/server/analysis/core/model/InteractorNode.java
+++ src/main/java/org/reactome/server/analysis/core/model/InteractorNode.java
@@ -1,23 +1,34 @@
 package org.reactome.server.analysis.core.model;
 
+import org.reactome.server.analysis.core.model.identifier.MainIdentifier;
+import org.reactome.server.analysis.core.util.MapSet;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * Interactors are not longer connected to a EntityNode. They are treated as main objects in the intermediate
+ * data structure.
+ *
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 public class InteractorNode {
 
     private String accession;
 
-    private Set<EntityNode> interactsWith;
+    private Set<MainIdentifier> interactsWith;
+
+    //We DO NOT use PathwayNode here because at some point cloning the hierarchies
+    //structure will be needed and keeping this separate will help to maintain the
+    //links between both structures easy through the pathway location map
+    private MapSet<Long, AnalysisReaction> pathwayReactions = null;
 
     public InteractorNode(String accession) {
         this.interactsWith = new HashSet<>();
         this.accession = accession;
     }
 
-    public void addInteractsWith(EntityNode interactsWith) {
+    public void addInteractsWith(MainIdentifier interactsWith) {
         this.interactsWith.add(interactsWith);
     }
 
@@ -25,8 +36,19 @@ public class InteractorNode {
         return accession;
     }
 
-    public Set<EntityNode> getInteractsWith() {
+    public Set<MainIdentifier> getInteractsWith() {
         return interactsWith;
+    }
+
+    public MapSet<Long, AnalysisReaction> getPathwayReactions() {
+        return pathwayReactions;
+    }
+
+    public void addPathwayReactions(MapSet<Long, AnalysisReaction> pathwayReactions) {
+        if (this.pathwayReactions == null) {
+            this.pathwayReactions = new MapSet<>();
+        }
+        this.pathwayReactions.addAll(pathwayReactions);
     }
 
     @Override

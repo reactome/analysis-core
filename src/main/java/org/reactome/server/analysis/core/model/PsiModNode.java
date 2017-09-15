53b36506dc773e73fc9a024c3c97b9455d94cc63
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-09-15 09:19:19 +0100

    PSI-MOD terms might have more than one parent. Code adjusted to this

--- src/main/java/org/reactome/server/analysis/core/model/PsiModNode.java
+++ src/main/java/org/reactome/server/analysis/core/model/PsiModNode.java
@@ -1,60 +1,36 @@
 package org.reactome.server.analysis.core.model;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class PsiModNode implements Comparable {
 
     private String identifier;
-    private PsiModNode parent;
-    private Set<PsiModNode> children;
+    private Set<PsiModNode> parents;
 
     public PsiModNode(String identifier) {
         this.identifier = identifier;
-        this.parent = null;
-        this.children = new HashSet<>();
-    }
-
-    public PsiModNode addChild(PsiModNode child){
-        PsiModNode rtn = null;
-        for (PsiModNode aux : children) {
-            if(aux.equals(child)){
-                rtn = aux;
-                break;
-            }
-        }
-
-        if(rtn == null){
-            children.add(child);
-            child.setParent(this);
-            rtn = child;
-        }
-
-        return rtn;
+        this.parents = null;
     }
 
     public String getIdentifier() {
         return identifier;
     }
 
-    public Map<String, PsiModNode> flatten() {
-        Map<String, PsiModNode> flat = new HashMap<>();
-        flat.put(identifier, this);
-        for (PsiModNode child : children) {
-            flat.putAll(child.flatten());
+    public void addParent(PsiModNode parent) {
+        if (this.parents == null) this.parents = new HashSet<>();
+        this.parents.add(parent);
+    }
+
+    public boolean isA(String identifier) {
+        if (this.identifier.equals(identifier)) {
+            return true;
+        } else if (parents != null) {
+            for (PsiModNode parent : parents) {
+                if (parent.isA(identifier)) return true;
+            }
         }
-        return flat;
-    }
-
-    private void setParent(PsiModNode parent) {
-        if(this.parent!=null) throw new RuntimeException("You cannot add more than one parent per tree node");
-        this.parent = parent;
-    }
-
-    public boolean isA(String identifier){
-        return this.identifier.equals(identifier) || (parent != null && parent.isA(identifier));
+        return false;
     }
 
     @Override

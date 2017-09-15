1230be50ba1fa422894602b9b8aff9d40c117da2
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-09-15 09:21:32 +0100

    PSI-MOD importer uses the PsiModGraph generator to build the ontology structure

--- src/main/java/org/reactome/server/analysis/core/importer/PsiModImporter.java
+++ src/main/java/org/reactome/server/analysis/core/importer/PsiModImporter.java
@@ -1,47 +1,63 @@
 package org.reactome.server.analysis.core.importer;
 
+import org.reactome.server.analysis.core.Main;
+import org.reactome.server.analysis.core.importer.psimod.PsiModGraph;
+import org.reactome.server.analysis.core.importer.psimod.Term;
 import org.reactome.server.analysis.core.model.PsiModNode;
 import org.reactome.server.graph.domain.model.PsiMod;
 import org.reactome.server.graph.service.SchemaService;
 import org.reactome.server.graph.utils.ReactomeGraphCore;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 public class PsiModImporter {
 
-    public static PsiModNode getPsiModTree() {
+    public static Map<String, PsiModNode> getPsiModMap() {
+
+        Map<String, PsiModNode> psiModNodeMap = new HashMap<>();
+
         SchemaService service = ReactomeGraphCore.getService(SchemaService.class);
 
-        PsiModNode rtn = new PsiModNode(null);
+        PsiModGraph graph = getPsiModGraph(service.getByClass(PsiMod.class));
 
-        Collection<PsiMod> psiMods = service.getByClass(PsiMod.class);
-        for (PsiMod psiMod : psiMods) {
-            PsiModNode psiModNode = rtn.flatten().get(psiMod.getIdentifier());
-            if (psiModNode == null) {
-                List<String> branch = getBranch(psiMod.getIdentifier());
-                Collections.reverse(branch);
-                PsiModNode aux = rtn;
-                for (String s : branch) {
-                    PsiModNode child = new PsiModNode(s);
-                    aux = aux.addChild(child);
-                }
-            }
+        for (Term term : graph.getRoot().getChildrenAll()) {
+            psiModNodeMap.put(term.getId(), new PsiModNode(term.getId()));
         }
 
-        return rtn;
+        for (Term termLeaf : graph.getRoot().getLeafs()) {
+            PsiModNode nodeLeaf = psiModNodeMap.get(termLeaf.getId());
+            fillParentsPath(termLeaf, nodeLeaf, psiModNodeMap);
+        }
+
+        return psiModNodeMap;
     }
 
-    private static List<String> getBranch(String identifier){
-        List<String> rtn = new ArrayList<>();
-        rtn.add(identifier);
-        //TODO: Query PSIMod to find out all the parents of psiMod and add them to the list
-        return rtn;
+    private static PsiModGraph getPsiModGraph(Collection<PsiMod> psiMods) {
+        PsiModGraph psiModGraph = new PsiModGraph();
+        String msgPrefix = "\rCreating the PSI-MOD ontology graph";
+        int i = 0;
+        int tot = psiMods.size();
+        for (PsiMod psiMod : psiMods) {
+            if (Main.VERBOSE) System.out.print(msgPrefix + " >> '" + psiMod.getDisplayName() + "' (" + (++i) + "/" + tot + ")");
+            psiModGraph.addTerm(psiMod.getIdentifier());
+        }
+        if (Main.VERBOSE) System.out.println(msgPrefix + " >> Done.");
+        return psiModGraph;
+    }
+
+    private static void fillParentsPath(Term term, PsiModNode node, Map<String, PsiModNode> psiModNodeMap) {
+        if (!term.isRoot()) {
+            for (Term termParent : term.getParents()) {
+                PsiModNode nodeParent = psiModNodeMap.get(termParent.getId());
+                node.addParent(nodeParent);
+                fillParentsPath(termParent, nodeParent, psiModNodeMap);
+            }
+        }
     }
 
 }

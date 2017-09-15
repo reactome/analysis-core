62c4d39f0b33a862f2c4b612a0f7612909f043de
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-09-15 13:46:09 +0100

    VERBOSE messages improved

--- src/main/java/org/reactome/server/analysis/core/data/AnalysisDataUtils.java
+++ src/main/java/org/reactome/server/analysis/core/data/AnalysisDataUtils.java
@@ -6,6 +6,7 @@ import com.esotericsoftware.kryo.io.Output;
 import com.esotericsoftware.shaded.org.objenesis.strategy.StdInstantiatorStrategy;
 import org.reactome.server.analysis.core.Main;
 import org.reactome.server.analysis.core.model.DataContainer;
+import org.reactome.server.analysis.core.util.FormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,27 +21,28 @@ import java.io.OutputStream;
 public class AnalysisDataUtils {
     private static Logger logger = LoggerFactory.getLogger(AnalysisDataUtils.class.getName());
 
-    public static DataContainer getDataContainer(InputStream file){
+    public static DataContainer getDataContainer(InputStream file) {
         logger.info(String.format("Loading %s file...", DataContainer.class.getSimpleName()));
         long start = System.currentTimeMillis();
         DataContainer container = (DataContainer) AnalysisDataUtils.read(file);
         long end = System.currentTimeMillis();
-        logger.info(String.format("%s file loaded in %d ms", DataContainer.class.getSimpleName() , end-start));
+        logger.info(String.format("Time to load %s file >> %s", DataContainer.class.getSimpleName(), FormatUtils.getTimeFormatted(end - start)));
         return container;
     }
 
-    public static <T> T kryoCopy(T object){
+    public static <T> T kryoCopy(T object) {
         long start = System.currentTimeMillis();
         Kryo kryo = new Kryo();
         kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
         T rtn = kryo.copy(object);
         long end = System.currentTimeMillis();
-        logger.trace(String.format("%s cloned in %d ms", object.getClass().getSimpleName() , end-start));
+        logger.trace(String.format("%s cloned in %d ms", object.getClass().getSimpleName(), end - start));
         return rtn;
     }
 
-    public static void kryoSerialisation(DataContainer container, String fileName){
-        if(Main.VERBOSE) System.out.println(String.format("Saving %s data into file %s", container.getClass().getSimpleName(), fileName));
+    public static void kryoSerialisation(DataContainer container, String fileName) {
+        String msgPrefix = String.format("\rStoring %s data into file %s", container.getClass().getSimpleName(), fileName);
+        if (Main.VERBOSE) System.out.print(msgPrefix + " >> Please wait...");
         try {
             Kryo kryo = new Kryo();
             kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
@@ -48,13 +50,15 @@ public class AnalysisDataUtils {
             Output output = new Output(file);
             kryo.writeClassAndObject(output, container);
             output.close();
+            if (Main.VERBOSE) System.out.println(msgPrefix + " >> Done.");
         } catch (FileNotFoundException e) {
+            if (Main.VERBOSE) System.out.println(msgPrefix + " >> An error has occurred: " + e.getMessage());
             logger.error(e.getMessage(), e);
             e.printStackTrace();
         }
     }
 
-    private static Object read(InputStream file){
+    private static Object read(InputStream file) {
         Kryo kryo = new Kryo();
         kryo.setInstantiatorStrategy(new StdInstantiatorStrategy());
         Input input = new Input(file);

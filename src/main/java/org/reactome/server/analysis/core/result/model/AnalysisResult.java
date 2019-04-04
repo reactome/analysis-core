502c31fde7edbd20c129281e0a3d0a10216f296e
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-04-04 10:55:05 +0100

    added speciesSummary

--- src/main/java/org/reactome/server/analysis/core/result/model/AnalysisResult.java
+++ src/main/java/org/reactome/server/analysis/core/result/model/AnalysisResult.java
@@ -8,27 +8,15 @@ import java.util.List;
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
-//@ApiModel(value = "AnalysisResult", description = "Contains general information about the result plus a list with the found pathways", discriminator = "", subTypes = {PathwaySummary.class})
 public class AnalysisResult {
-//    @ApiModelProperty(value = "Token associated with the query", notes = "Is a good practise to use it with the /token method for future filters of the result", required = true)
 
     private AnalysisSummary summary;
-
     private ExpressionSummary expression;
-
-//    @ApiModelProperty(value = "Number of identifiers in the sample without entities associated in the Reactome database", required = true)
     private Integer identifiersNotFound;
-
-    //TODO: Add found identifiers number
-
-//    @ApiModelProperty(value = "Number of pathways found with hits for the given sample", required = true)
     private Integer pathwaysFound;
-
-//    @ApiModelProperty(value = "List of pathways in which the input has been found", required = true) // dataType = "PathwaySummary" ) //, notes = "It may contain a subset of the pathways depending of the filtering options" )
     private List<PathwaySummary> pathways;
-
     private List<ResourceSummary> resourceSummary;
-
+    private List<SpeciesSummary> speciesSummary;
     private List<String> warnings;
 
     public AnalysisResult(AnalysisStoredResult storedResult, List<PathwaySummary> pathways){
@@ -39,6 +27,7 @@ public class AnalysisResult {
         this.expression = storedResult.getExpressionSummary();
         this.resourceSummary = storedResult.getResourceSummary();
         this.warnings = storedResult.getWarnings();
+        this.speciesSummary = storedResult.getSpeciesSummary();
     }
 
     public AnalysisSummary getSummary() {
@@ -63,6 +52,10 @@ public class AnalysisResult {
 
     public List<ResourceSummary> getResourceSummary() {
         return resourceSummary;
+    }
+
+    public List<SpeciesSummary> getSpeciesSummary() {
+        return speciesSummary;
     }
 
     public ExpressionSummary getExpression() {

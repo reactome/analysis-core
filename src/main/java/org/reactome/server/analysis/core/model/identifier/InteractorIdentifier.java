bbfcdb8ef73fa079b027457ca25b1fd2731d1049
Antonio Fabregat <fabregat.antonio@gmail.com>
2019-04-04 10:54:35 +0100

    added equals and hashCode methods

--- src/main/java/org/reactome/server/analysis/core/model/identifier/InteractorIdentifier.java
+++ src/main/java/org/reactome/server/analysis/core/model/identifier/InteractorIdentifier.java
@@ -2,6 +2,8 @@ package org.reactome.server.analysis.core.model.identifier;
 
 import org.reactome.server.analysis.core.model.AnalysisIdentifier;
 
+import java.util.Objects;
+
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
@@ -18,11 +20,26 @@ public class InteractorIdentifier extends AnalysisIdentifier {
         this(identifier);
         this.mapsTo = mapsTo;
     }
+
     public InteractorIdentifier(AnalysisIdentifier identifier) {
         super(identifier.getId(), identifier.getExp());
     }
 
     public String getMapsTo() {
         return mapsTo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        InteractorIdentifier that = (InteractorIdentifier) o;
+        return Objects.equals(mapsTo, that.mapsTo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), mapsTo);
     }
 }

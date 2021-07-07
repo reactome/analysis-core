812c845f3925ec64b5b97d427d1ece0ba0035613
cqgong <cgong@ebi.ac.uk>
2021-07-07 12:25:28 +0100

    add @EnableAutoConfiguration to AnalysisCoreNeo4jConfig

--- src/main/java/org/reactome/server/analysis/core/config/AnalysisCoreNeo4jConfig.java
+++ src/main/java/org/reactome/server/analysis/core/config/AnalysisCoreNeo4jConfig.java
@@ -3,6 +3,7 @@ package org.reactome.server.analysis.core.config;
 import org.aspectj.lang.Aspects;
 import org.reactome.server.graph.aop.LazyFetchAspect;
 import org.reactome.server.graph.config.GraphCoreNeo4jConfig;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = {"org.reactome.server.graph.repository"})
 @EnableSpringConfigured
+@EnableAutoConfiguration
 public class AnalysisCoreNeo4jConfig extends GraphCoreNeo4jConfig {
     /**
      * This is needed to get hold of the instance of the aspect which is created outside of the spring container,
